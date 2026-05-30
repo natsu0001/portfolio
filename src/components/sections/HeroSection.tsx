f456eec82887a1a063bae9abc91324774f69d451
natsu0001 <talukdarabhishek687@gmail.com>
2026-05-30 12:31:45 +0530

    heroimg

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,5 +1,5 @@
 import Container from "../layout/Container";
-import SectionWrapper from "../layout/SectionWrapper";
+import heroImage from "@/assets/hero.png";
 
 const HeroSection = () => {
   return (
@@ -19,17 +19,19 @@ const HeroSection = () => {
 
       <div className="flex-1 flex flex-col items-center justify-center">
 
-              <div>
+              <div className="relative">
                 <img
-                 src="/hero-image.png"
+                 src={heroImage}
                  alt="Abhishek"
                  className="max-w-md w-full"
                  />
-              </div>
-              <div className= "flex md:hidden gap-4 mt-4">
+
+                 <div className= "absolute bottom-4 left-1/2 -translate-x-1/2 flex flex-col md:hidden gap-4">
               <button>view my work</button>
               <button>download resume</button>
               </div>
+              </div>
+              
 
       </div>
 
