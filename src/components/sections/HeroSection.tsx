b5c37f8a43c0f8ddd695b589a04221db2dded94b
natsu0001 <talukdarabhishek687@gmail.com>
2026-05-30 11:46:41 +0530

    hero lay

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -3,16 +3,34 @@ import SectionWrapper from "../layout/SectionWrapper";
 
 const HeroSection = () => {
   return (
-    <div>
-      <div>fullStack Devloper</div>
-      <div> I BUILD<br />DIGITAL<br />EXPERIENCES</div>
-      <div>Through code, motion and storytelling.</div>
-      <div className= "hidden">I'm a fullstack developer and visual storyteller passionate about building
-          scalable web applications and capturing real moments.</div>
-      <div>image</div>
-      <div>
-        <button>view my work</button>
-        <button>download resume</button>
+    <div className="flex flex-col md:flex-row items-center  min-h-screen">
+       <div className= "flex-1">
+         <div>fullStack Devloper</div>
+         <div> I BUILD<br />DIGITAL<br />EXPERIENCES</div>
+         <div>Through code, motion and storytelling.</div>
+         <div className= "hidden md:block">I'm a fullstack developer and visual storyteller passionate about building
+          scalable web applications and capturing real moments.
+         </div>
+         <div className="hidden md:flex gap-4 mt-4">
+         <button>view my work</button>
+         <button>download resume</button>
+        </div>
+      </div>
+
+      <div className="flex-1 flex flex-col items-center justify-center">
+
+              <div>
+                <img
+                 src="/hero-image.png"
+                 alt="Abhishek"
+                 className="max-w-md w-full"
+                 />
+              </div>
+              <div className= "flex md:hidden gap-4 mt-4">
+              <button>view my work</button>
+              <button>download resume</button>
+              </div>
+
       </div>
 
     </div>
