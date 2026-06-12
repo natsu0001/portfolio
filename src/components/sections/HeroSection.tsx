a18767eb580936b1502bb583543ea71bf5717a71
natsu0001 <talukdarabhishek687@gmail.com>
2026-06-12 14:31:24 +0530

    sm fixxed

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -15,11 +15,13 @@ const HeroSection = () => {
           
           flex
           flex-col
-          md:flex-row
+          justify-center
+          
+          sm:flex-row
           
          gap-8
          lg:gap-12
-          items-start
+          
           
     
         "
@@ -36,7 +38,7 @@ const HeroSection = () => {
               text-white
               leading-[0.9]
               mb-4
-              text-[2rem]
+              text-[3rem]
               sm:text-[2.5rem]
               md:text-[clamp(3.5rem,5vw,4.5rem)]
               
@@ -66,7 +68,7 @@ const HeroSection = () => {
 
           <p
             className="
-            hidden md:block
+            hidden lg:block
               text-sm
               leading-[1.8]
               text-text-2
@@ -81,7 +83,7 @@ const HeroSection = () => {
           </p>
 
           {/* Desktop Buttons */}
-          <div className="hidden md:flex gap-4 max-w-[450px]">
+          <div className="hidden sm:flex gap-4 max-w-[450px]">
             <Button
               className="flex-1 h-12 uppercase tracking-[0.15em]"
               variant="primary"
@@ -100,14 +102,14 @@ const HeroSection = () => {
 
         {/* Right Image */}
         <div className="flex-1 flex justify-center">
-          <div className="relative">
+          <div className="relative isolate ">
             <img
               src={heroImage}
               alt="Abhishek"
               className="
                 w-full
-                max-w-[320px]
-                sm:max-w-sm
+                max-w-[400px]
+                sm:max-w-[250px]
                 md:max-w-md
                   
                 
@@ -116,7 +118,7 @@ const HeroSection = () => {
             />
 
             {/* Mobile Gradient Overlay */}
-            <div className="absolute inset-x-0 bottom-0 h-48 sm:h-72 bg-gradient-to-t from-black via-black/60 to-transparent md:hidden">
+            <div className="absolute inset-x-0 bottom-0 h-48 sm:h-72 bg-gradient-to-t from-black via-black/60 to-transparent sm:hidden">
               <div className="absolute bottom-4 left-1/2 -translate-x-1/2 w-[90%] max-w-sm flex flex-col gap-4">
                 <Button
                   className="h-12 uppercase tracking-[0.15em]"
