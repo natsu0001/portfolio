d4460bd127da0d4067ad9e4e04a874947b6c8214
natsu0001 <talukdarabhishek687@gmail.com>
2026-06-17 11:23:24 +0530

    edu ts

---
+++ src/data/education.ts
@@ -0,0 +1,28 @@
+export const education = [
+  {
+    id: 1,
+    degree: "Computer Science and Engineering",
+    institution: "Assam Don Bosco University",
+    duration: "2020 — 2024",
+    description:
+      "Focused on software development, databases, algorithms and modern web technologies.",
+  },
+
+  {
+    id: 2,
+    degree: "Higher Secondary Education",
+    institution: "Saint Francis D'Assisi High School",
+    duration: "2018 — 2020",
+    description:
+      "Science , Mathematics , Physics, Chemistry .",
+  },
+
+  {
+    id: 3,
+    degree: "St. Clare's Convent Senior Secondary School",
+    institution: "St. clares Convent high School",
+    duration: "2014 — 2018",
+    description:
+      "Science , Mathematics , Physics, Chemistry , Literature and Social Studies .",
+  }
+];
