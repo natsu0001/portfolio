e420d3b3eac0e1872f6e3d1ff4d788d1afbee5c9
natsu0001 <talukdarabhishek687@gmail.com>
2026-06-29 09:58:29 +0530

    animaIndex

---
+++ src/animations/index.ts
@@ -0,0 +1,5 @@
+export { default as FadeLeft } from "./FadeLeft";
+export { default as FadeRight } from "./FadeRight";
+export { default as FadeUp } from "./FadeUp";
+export { default as FadeDown } from "./FadeDown";
+export { default as Stagger } from "./Stagger";
